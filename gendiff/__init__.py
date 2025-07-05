7e85e72ce7fb8648dd79b7d2b7165f1257c46d8d
Igor A. <justlikeme11@mail.ru>
2025-07-05 17:02:50 +0300

    add format

---
+++ gendiff/__init__.py
@@ -0,0 +1,5 @@
+from gendiff.scripts.gendiff import main
+
+__all__ = (
+  'main'
+  )
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,10 +3,12 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(
     prog='gendiff',
-    usage='gendiff [-h] first_file second_file',
+    usage='gendiff [-h] [-f FORMAT] first_file second_file',
     description='''Compares two configuration files and shows a difference.''')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output', metavar='FORMAT')
+    parser.print_help()
 
 
 if __name__ == "__main__":
