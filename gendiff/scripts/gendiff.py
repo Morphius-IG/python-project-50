edfe2fe9b8a4b2f5c2c83fe65a1b09daa1c5969a
Igor A. <justlikeme11@mail.ru>
2025-07-06 15:48:56 +0300

    add json comparison

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,29 @@
 import argparse
 import json
 
+def parse_files(file_path1, file_path2):
+    return json.load(open(file_path1)), json.load(open(file_path2))
+
+def generate_diff(data1, data2):
+    keys = sorted(set(data1.keys()) | set(data2.keys()))
+    diff = {}
+    
+    for key in keys:
+        if key not in data1:
+            diff[f"+ {key}"] = data2[key]
+        elif key not in data2:
+            diff[f"- {key}"] = data1[key]
+        elif data1[key] != data2[key]:
+            diff[f"- {key}"] = data1[key]
+            diff[f"+ {key}"] = data2[key]
+        else:
+            diff[key] = data1[key]
+    result = ''
+    for key, value in diff.items():
+        result += f'\n{key}: {value}'
+    return result
+    
+
 def main():
     parser = argparse.ArgumentParser(
     prog='gendiff',
@@ -9,16 +32,11 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output', metavar='FORMAT')
-    first_file = json.load(open('gendiff/files/file1.json'))
-    second_file = json.load(open('gendiff/files/file2.json'))
     args = parser.parse_args() 
-    #parser.print_help()
 
-
-
-# …
-
-
+    file1_data, file2_data = parse_files(args.first_file, args.second_file)
+    diff = generate_diff(file1_data, file2_data)
+    return diff
 
 
 if __name__ == "__main__":
