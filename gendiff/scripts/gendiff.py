6bd8a029c8b1c630376155344c2360fd59b21596
Igor A. <justlikeme11@mail.ru>
2025-07-06 16:02:16 +0300

    add check fron hexlet

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,10 @@
 import argparse
 import json
 
+
 def parse_files(file_path1, file_path2):
     return json.load(open(file_path1)), json.load(open(file_path2))
+
 
 def generate_diff(data1, data2):
     keys = sorted(set(data1.keys()) | set(data2.keys()))
@@ -31,7 +33,9 @@ def main():
     description='''Compares two configuration files and shows a difference.''')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output', metavar='FORMAT')
+    parser.add_argument('-f', '--format', 
+                        help='set format of output', 
+                        metavar='FORMAT')
     args = parser.parse_args() 
 
     file1_data, file2_data = parse_files(args.first_file, args.second_file)
