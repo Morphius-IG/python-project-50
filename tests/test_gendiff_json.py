d7fde0f3efaf5035d8e71662c624369f8947998f
Igor A. <justlikeme11@mail.ru>
2025-07-16 20:13:35 +0300

    test+

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -4,15 +4,13 @@ import json
 
 def test_generate_diff():
   file1, file2 = json.load(open('gendiff/files/file1.json')), json.load(open('gendiff/files/file2.json'))
-  assert generate_diff(file1, file2) == '''
-- follow: False
+  assert generate_diff(file1, file2) == '''- follow: False
 host: hexlet.io
 - proxy: 123.234.53.22
 - timeout: 50
 + timeout: 20
 + verbose: True'''
-  assert generate_diff(file1, file1) == '''
-follow: False
+  assert generate_diff(file1, file1) == '''follow: False
 host: hexlet.io
 proxy: 123.234.53.22
 timeout: 50'''
--- tests/test_gendiff_yaml.py
+++ tests/test_gendiff_yaml.py
@@ -4,15 +4,13 @@ import yaml
 
 def test_generate_diff():
   file1, file2 = yaml.safe_load(open('gendiff/files/file1.yml')), yaml.safe_load(open('gendiff/files/file2.yml'))
-  assert generate_diff(file1, file2) == '''
-- follow: False
+  assert generate_diff(file1, file2) == '''- follow: False
 host: hexlet.io
 - proxy: 123.234.53.22
 - timeout: 50
 + timeout: 20
 + verbose: True'''
-  assert generate_diff(file1, file1) == '''
-follow: False
+  assert generate_diff(file1, file1) == '''follow: False
 host: hexlet.io
 proxy: 123.234.53.22
 timeout: 50'''
