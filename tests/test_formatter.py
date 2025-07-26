c7ec10d426501c9531fed9e55940c1a903d346fc
Igor A. <justlikeme11@mail.ru>
2025-07-26 18:16:18 +0300

    +tests

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -53,3 +53,70 @@ def test_to_stylish():
     }
 }'''
   assert to_stylish(diff) == expected
+
+
+def test_to_stylish_flat_dict():
+    data = {
+        "key1": {"status": "added", "value": "value1"},
+        "key2": {"status": "deleted", "value": "value2"},
+    }
+    expected = (
+        "{\n"
+        "  + key1: value1\n"
+        "  - key2: value2\n"
+        "}"
+    )
+    assert to_stylish(data) == expected
+
+
+def test_to_stylish_nested_dict():
+    data = {
+        "parent": {
+            "status": "parent",
+            "value": {
+                "child": {"status": "added", "value": "child_value"}
+            }
+        }
+    }
+    expected = (
+        "{\n"
+        "    parent: {\n"
+        "      + child: child_value\n"
+        "    }\n"
+        "}"
+    )
+    assert to_stylish(data) == expected
+
+
+def test_to_stylish_changed_value():
+    data = {
+        "key": {
+            "status": "changed",
+            "old": "old_value",
+            "new": "new_value"
+        }
+    }
+    expected = (
+        "{\n"
+        "  - key: old_value\n"
+        "  + key: new_value\n"
+        "}"
+    )
+    assert to_stylish(data) == expected
+
+
+def test_to_stylish_not_changed_value():
+    data = {
+        "key": {"status": "not changed", "value": "same_value"}
+    }
+    expected = (
+        "{\n"
+        "    key: same_value\n"
+        "}"
+    )
+    assert to_stylish(data) == expected
+
+
+
+
+
