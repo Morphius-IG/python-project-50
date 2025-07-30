e8cca7ffcfb0ed6a207d17c1d286bb48cdcb9bc6
Igor A. <justlikeme11@mail.ru>
2025-07-30 13:46:22 +0300

    +++++

--- tests/test_stylish.py
+++ tests/test_stylish.py
@@ -1,59 +1,5 @@
 from gendiff.scripts.formatters.stylish import to_stylish
 from gendiff.gendiff import generate_diff
-import json
-
-
-def test_to_stylish():
-  file1 = (json.load(open('gendiff/files//tree1.json')))
-  file2 = (json.load(open('gendiff/files//tree2.json')))
-  diff = generate_diff(file1, file2) 
-
-  expected = '''{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}'''
-  assert to_stylish(diff) == expected
-
 
 def test_simple_added():
     data = {"key": {"status": "added", "value": "value"}}
